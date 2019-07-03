5d5db3527efcee1275e781085d2e74f712d415e5
Przemek Drewa <przemyslaw.drewa@gmail.com>
2019-07-03 21:33:48 +0200

    AirportService created and filled

---
+++ src/main/java/com/sda/travel_agency/service/AirportService.java
@@ -0,0 +1,41 @@
+package com.sda.travel_agency.service;
+
+import com.sda.travel_agency.entity.Airport;
+import com.sda.travel_agency.entity.City;
+import com.sda.travel_agency.repository.AirportRepository;
+import com.sda.travel_agency.repository.CityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+
+public class AirportService {
+
+    private final AirportRepository airportRepository;
+    private final CityService cityService;
+
+    public void createOrUpdateAirportForCity(Airport airport, Long cityId) {
+
+        Optional<City> city = cityService.findCityById(cityId);
+            city.ifPresent(theCity -> airport.setCity(theCity));
+            airportRepository.save(airport);
+    }
+
+    public Iterable<Airport> getAllAirport() {return airportRepository.findAll();}
+
+    public List<Airport> findAllByAirportNameConatining(String airportName) {
+        return airportRepository.findByAirportNameContaining(airportName);
+    }
+
+    public void deleteById (Long id) {airportRepository.deleteById(id);}
+
+    public Optional<Airport> findAirportById (Long airportId) {return airportRepository.findById(airportId);}
+
+
+
+
+}

d66b4d101a10b4cd0d36c31b01de10acf83249c0
Przemek Drewa <przemyslaw.drewa@gmail.com>
2019-07-08 20:50:03 +0200

    Admin authentication

---
+++ src/main/java/com/sda/travel_agency/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.sda.travel_agency.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").authenticated().and()
+                .rememberMe().tokenValiditySeconds(86400).and()
+                .httpBasic();
+    }}
